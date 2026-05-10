232d0361e5ee38675eeb52be66a20f5a0c8fa180
whellcolor <bahansatekambing@gmail.com>
2026-05-10 10:47:17 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,3 +11,19 @@ module.exports = {
     },
   },
 };
+
+
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.27",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
